aa2b8b60ff5fa32be84d287e6eb1d69bc3c79440
rubyyot <jamal.hansen@gmail.com>
2010-03-22 15:33:49 -0500

    don't try to write blob if it's nil

--- lib/rack-git-up.rb
+++ lib/rack-git-up.rb
@@ -3,7 +3,7 @@ require 'rack/file'
 
 module Rack
   class GitUp
- 
+
     def initialize(app, options={})
       @app = app
       @repo = Grit::Repo.new(options[:repo])
@@ -11,22 +11,25 @@ module Rack
       @root = options[:root] || Dir.pwd
       @file_server = Rack::File.new(@root)
     end
- 
+
     def call(env)
       path = env["PATH_INFO"]
       can_serve = @urls.any? { |url| path.index(url) == 0 }
- 
+
       if can_serve
         pull_from_git path
         result = @file_server.call(env)
         return result if result[0] == 200
       end
-      
+
       @app.call(env)
     end
 
     def pull_from_git path
       blob = (@repo.tree/path[1..-1])   #remove leading slash
+
+      return unless blob
+
       f = ::File.new(::File.join(@root, path), "w")
       f.syswrite(blob.data)
       f.close
